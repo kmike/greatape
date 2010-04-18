75527abafe42e418176b2380fd1addf43376039d
Jonathan Lukens <jonathan@system76-pc.(none)>
2010-04-18 14:06:57 -0400

    Bumped version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "greatape",
-    version = "0.1",
+    version = "0.2.0",
     url = 'http://github.com/threadsafelabs/greatape',
     license = 'BSD',
     description = "A MailChimp API client.",
