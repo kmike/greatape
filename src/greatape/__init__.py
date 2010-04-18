57dc22fbb4ff64349de6f824ce00190df47c1404
Jonathan Lukens <jonathan@system76-pc.(none)>
2010-04-18 13:56:14 -0400

    Fixed handling of array and dictionary parameter values.

--- src/greatape/__init__.py
+++ src/greatape/__init__.py
@@ -1,4 +1,4 @@
-import urllib
+from urllib import urlencode, quote_plus
 import urllib2
 from functools import partial
 
@@ -27,12 +27,12 @@ class MailChimp(object):
         params = {
             'output': 'json',
             'apikey': self.api_key,
-            'method': kwargs['method']
+            'method': kwargs.pop('method')
         }
-        params.update(kwargs)
+        querystring = '%s&%s' % (urlencode(params), self._serialize(kwargs))
         req = urllib2.Request(
                 "https://%s.api.mailchimp.com/1.2/?%s" % (
-                    self.data_center, urllib.urlencode(params)))
+                    self.data_center, querystring))
         try:
             handle = urllib2.urlopen(req)
             return json.loads(handle.read())
@@ -41,6 +41,27 @@ class MailChimp(object):
                 return []
             else:
                 raise MailChimpError
+
+    def _serialize(self, params, key=None):
+        """Replicates PHP's (incorrect) serialization to query parameters to
+        accommodate the "array-based" parameters of MailChimp API methods.
+        """
+        pairs = []
+        try:
+            items = params.items()
+        except AttributeError:
+            items = [(str(i), n) for i, n in enumerate(params)]
+        for name, value in items:
+            name = quote_plus(name)
+            if key is not None:
+                name = '%s[%s]' % (key, name)
+            if type(value) in (list, dict):
+                pairs.append(self._serialize(value, name))        
+            elif value is not None:
+                if type(value) == bool:
+                    value = str(value).lower()
+                pairs.append('%s=%s' % (name, quote_plus(value)))
+        return '&'.join(pairs)
 
 
 __all__ = ["MailChimp", "MailChimpError"]
