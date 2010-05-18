2ea5251f65309d20ec3e405c12018809c40811bb
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-05-18 01:06:03 -0400

    Added Python 2.4 support.  Thanks to ccedgordon for the patch.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "greatape",
-    version = "0.2.0",
+    version = "0.2.2",
     url = 'http://github.com/threadsafelabs/greatape',
     license = 'BSD',
     description = "A MailChimp API client.",
--- src/greatape/__init__.py
+++ src/greatape/__init__.py
@@ -1,6 +1,21 @@
 from urllib import urlencode, quote_plus
 import urllib2
-from functools import partial
+
+
+try:
+    import functools
+    partial = functools.partial
+except ImportError:
+    def partial(func, *args, **keywords):
+        def newfunc(*fargs, **fkeywords):
+            newkeywords = keywords.copy()
+            newkeywords.update(fkeywords)
+            return func(*(args + fargs), **newkeywords)
+        newfunc.func = func
+        newfunc.args = args
+        newfunc.keywords = keywords
+        return newfunc
+
 
 try:
     import json
